6116431830fdce5b53926abe5025b0a86f3bfcbc
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-05-11 05:47:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,12 @@ def join_nested_strings(src)
   while count < src.length do
     inner_count = 0 
     while inner_count < src[count].length do 
-      if src[count][inner_count].is_a? == 
+      if src[count][inner_count].class == String
+        joined_string = joined_string + src[count][inner_count]
+      end 
+      inner_count += 1 
     end 
+    count += 1 
   end 
+  joined_string
 end
